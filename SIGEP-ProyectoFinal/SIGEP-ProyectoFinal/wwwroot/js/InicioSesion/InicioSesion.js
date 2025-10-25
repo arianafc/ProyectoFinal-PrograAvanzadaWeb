64cfb1586f558ccb47aaf575ebb2550fca38b9ba
arianafc <123421110+arianafc@users.noreply.github.com>
2025-10-25 17:58:47 -0600

    Update

---
+++ SIGEP-ProyectoFinal/SIGEP-ProyectoFinal/wwwroot/js/InicioSesion/InicioSesion.js
@@ -0,0 +1,53 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+
+    const toggle = document.querySelector('.toggle-password');
+    const input = document.querySelector('.password-input');
+
+    if (toggle && input) {
+        toggle.addEventListener('click', function () {
+            const type = input.type === 'password' ? 'text' : 'password';
+            input.type = type;
+            toggle.classList.toggle('fa-eye');
+            toggle.classList.toggle('fa-eye-slash');
+        });
+    }
+
+
+    $("#loginForm").on("submit", function (e) {
+        e.preventDefault();
+
+        let cedula = $("#cedulaLogin").val().trim();
+        let contrasenna = $("#contrasenna").val().trim();
+
+        if (!cedula || !contrasenna) {
+            Swal.fire({
+                icon: "warning",
+                title: "Campos vacíos",
+                text: "Por favor, completa ambos campos para continuar.",
+                confirmButtonColor: "#2D594D"
+            });
+            return;
+        }
+
+        this.submit();
+    });
+
+    const errorMsg = document.getElementById("TempError")?.value;
+    const successMsg = document.getElementById("TempSuccess")?.value;
+
+    if (errorMsg) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: errorMsg,
+            confirmButtonColor: '#2D594D'
+        });
+    } else if (successMsg) {
+        Swal.fire({
+            icon: 'success',
+            title: 'Éxito',
+            text: successMsg,
+            confirmButtonColor: '#2D594D'
+        });
+    }
+});
